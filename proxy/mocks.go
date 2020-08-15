242a9867a8c6d4fb554ea6ca2f3ab0334e9e68ac
andres perez <andresperez0828@gmail.com>
2020-08-15 09:01:17 +0200

    adding comments in mocks

--- proxy/mocks.go
+++ proxy/mocks.go
@@ -5,35 +5,43 @@ import (
 	"net/http"
 )
 
+// CustomResponseWriter implements http.ResponseWriter
 type CustomResponseWriter struct {
 	Headers http.Header
 	Status  int
 	Body    []byte
 }
 
+// Write ...
 func (w *CustomResponseWriter) Write(b []byte) (int, error) {
 	w.Body = b
 	return 1, nil
 }
 
+// Header ...
 func (w *CustomResponseWriter) Header() http.Header {
 	return w.Headers
 }
 
+// WriteHeader ...
 func (w *CustomResponseWriter) WriteHeader(s int) {
 	w.Status = s
 }
 
+// NewCustomeRsWriter ...
 func NewCustomeRsWriter() http.ResponseWriter {
 	return &CustomResponseWriter{Headers: make(http.Header)}
 }
 
+// FakeReader feake reader implements the io.Reader and io.ReadCloser
 type FakeReader struct{}
 
+// Read ...
 func (r *FakeReader) Read(p []byte) (n int, err error) {
 	return 0, errors.New("error from fake reader")
 }
 
+// Close ...
 func (r *FakeReader) Close() error {
 	return nil
 }
